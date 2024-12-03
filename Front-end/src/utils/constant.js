6f93cc83579e86ec794cb1a9bd4c77e7d92f28d7
Abhishek Jadhav <abhiijadhavv@gmail.com>
2024-12-03 14:41:19 +0530

    minor change

--- Front-end/src/utils/constant.js
+++ Front-end/src/utils/constant.js
@@ -1,2 +1,3 @@
 export const USER_API_END_POINT="https://web-story-platform-by-abhishek.onrender.com/api/v1/user";
 export const STORY_API_END_POINT="https://web-story-platform-by-abhishek.onrender.com/api/v1/story";
+//update
