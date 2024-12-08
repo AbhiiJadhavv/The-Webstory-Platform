047c9adb4f101a12ded9a2de46b316216ea5805a
Abhishek Jadhav <abhiijadhavv@gmail.com>
2024-12-09 03:06:09 +0530

    minor change

--- Front-end/src/pages/Bookmarks.jsx
+++ Front-end/src/pages/Bookmarks.jsx
@@ -1,16 +1,14 @@
 import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar';
 import "../styles/Bookmarks.css";
-import homeIcon from "../assets/homeIcon.png";
 import Story from '../components/Story';
-import { useNavigate } from 'react-router-dom';
 import AddStory from '../components/AddStory';
+import NavbarMobile from '../components/NavbarMobile';
 
 const Bookmarks = (user, setUser) => {
   const [showStory, setShowStory] = useState(false);
   const [addStory, setAddStory] = useState(false);
   const bookmarks = [];
-  const navigate = useNavigate();
   const [isMobileView, setIsMobileView] = useState(window.innerWidth <= 780);
 
   useEffect(() => {
@@ -38,7 +36,6 @@ const Bookmarks = (user, setUser) => {
         <AddStory setAddStory={setAddStory} />
       )}
       <div className='bookmarksCon'>
-        <button onClick={() => navigate("/")} className='homeBtn'><img src={homeIcon} alt="home" /></button>
         <p className='yourBookmarksPara'>Your Bookmarks</p>
         {
           !bookmarks || bookmarks.length === 0 ? (
